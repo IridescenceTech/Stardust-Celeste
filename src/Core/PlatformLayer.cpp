cff6753fd7c091ca9bc41ffb654554d14bd50455
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2021-12-20 14:14:42 -0500

    Platform Layer

---
+++ src/Core/PlatformLayer.cpp
@@ -0,0 +1,13 @@
+#include <Core/PlatformLayer.hpp>
+
+namespace Stardust_Celeste::Core {
+
+  auto PlatformLayer::initialize(const AppConfig &app) -> void {
+      //TODO: Add layers as needed 
+  }
+
+  auto PlatformLayer::terminate() -> void {
+      //TODO: Add layers as needed
+  }
+
+}
