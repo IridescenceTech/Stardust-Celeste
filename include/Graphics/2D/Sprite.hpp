556f23c6694606f77ea75fbd89146580853d51ec
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-07-01 19:06:27 -0400

    Add selection set for Sprite

--- include/Graphics/2D/Sprite.hpp
+++ include/Graphics/2D/Sprite.hpp
@@ -2,9 +2,11 @@
 #include <Rendering/Mesh.hpp>
 #include <Utilities/Types.hpp>
 
-namespace Stardust_Celeste::Graphics::G2D {
+namespace Stardust_Celeste::Graphics::G2D
+{
 
-class Sprite {
+  class Sprite
+  {
   public:
     Sprite(u32 texture, Rendering::Rectangle bounds);
     Sprite(u32 texture, Rendering::Rectangle bounds,
@@ -17,6 +19,7 @@ class Sprite {
     auto set_position(glm::vec2 position) -> void;
     auto set_size(glm::vec2 position) -> void;
     auto set_rect(Rendering::Rectangle bounds) -> void;
+    auto set_selection(Rendering::Rectangle bounds) -> void;
     auto set_layer(s16 layer) -> void;
 
   protected:
@@ -30,6 +33,6 @@ class Sprite {
     ScopePtr<Rendering::Mesh> mesh;
     Rendering::Vertex *verts;
     u16 *idxs;
-};
+  };
 
 } // namespace Stardust_Celeste::Graphics::G2D
--- src/Graphics/2D/Sprite.cpp
+++ src/Graphics/2D/Sprite.cpp
@@ -1,109 +1,128 @@
 #include <Graphics/2D/Sprite.hpp>
 #include <Rendering/Texture.hpp>
 
-namespace Stardust_Celeste::Graphics::G2D {
+namespace Stardust_Celeste::Graphics::G2D
+{
 
-Sprite::Sprite(u32 tex, Rendering::Rectangle bnd) {
-    texture = tex;
-    bounds = bnd;
-    selection = Rendering::Rectangle{{0, 0}, {1, 1}};
+    Sprite::Sprite(u32 tex, Rendering::Rectangle bnd)
+    {
+        texture = tex;
+        bounds = bnd;
+        selection = Rendering::Rectangle{{0, 0}, {1, 1}};
 
-    verts = nullptr;
-    idxs = nullptr;
+        verts = nullptr;
+        idxs = nullptr;
 
-    layer = 0;
-    update_mesh();
-}
-
-Sprite::Sprite(u32 tex, Rendering::Rectangle bnd, Rendering::Rectangle sel) {
-    texture = tex;
-    bounds = bnd;
-    selection = sel;
-
-    verts = nullptr;
-    idxs = nullptr;
-
-    update_mesh();
-}
-
-Sprite::~Sprite() {
-    delete[] verts;
-    delete[] idxs;
-}
-
-auto Sprite::update(double dt) -> void {
-    // For base: do nothing.
-}
-
-auto Sprite::draw() -> void {
-    Rendering::TextureManager::get().bind_texture(texture);
-    mesh->draw();
-}
-
-auto Sprite::set_position(glm::vec2 position) -> void {
-    bounds.position = position;
-    update_mesh();
-}
-
-auto Sprite::set_size(glm::vec2 size) -> void {
-    bounds.extent = size;
-    update_mesh();
-}
-
-auto Sprite::set_rect(Rendering::Rectangle bnd) -> void {
-    bounds = bnd;
-    update_mesh();
-}
-
-auto Sprite::set_layer(s16 layer) -> void {
-    this->layer = layer;
-    update_mesh();
-}
-
-auto Sprite::update_mesh() -> void {
-    if (verts == nullptr)
-        verts = new Rendering::Vertex[4];
-
-    Rendering::Color white = {0xff, 0xff, 0xff, 0xff};
-
-    verts[0] = Rendering::Vertex{
-        selection.position.x, selection.position.y, white,
-        bounds.position.x,    bounds.position.y,    (float)layer};
-    verts[1] = Rendering::Vertex{selection.position.x + selection.extent.x,
-                                 selection.position.y,
-                                 white,
-                                 bounds.position.x + bounds.extent.x,
-                                 bounds.position.y,
-                                 (float)layer};
-    verts[2] = Rendering::Vertex{selection.position.x + selection.extent.x,
-                                 selection.position.y + selection.extent.y,
-                                 white,
-                                 bounds.position.x + bounds.extent.x,
-                                 bounds.position.y + bounds.extent.y,
-                                 (float)layer};
-    verts[3] = Rendering::Vertex{selection.position.x,
-                                 selection.position.y + selection.extent.y,
-                                 white,
-                                 bounds.position.x,
-                                 bounds.position.y + bounds.extent.y,
-                                 (float)layer};
-
-    if (idxs == nullptr) {
-        idxs = new u16[6];
-        idxs[0] = 0;
-        idxs[1] = 1;
-        idxs[2] = 2;
-        idxs[3] = 2;
-        idxs[4] = 3;
-        idxs[5] = 0;
+        layer = 0;
+        update_mesh();
     }
 
-    if (mesh.get() == nullptr)
-        mesh = create_scopeptr<Rendering::Mesh>(verts, 4, idxs, 6);
-    else {
-        mesh->delete_data();
-        mesh->add_data(verts, 4, idxs, 6);
+    Sprite::Sprite(u32 tex, Rendering::Rectangle bnd, Rendering::Rectangle sel)
+    {
+        texture = tex;
+        bounds = bnd;
+        selection = sel;
+
+        verts = nullptr;
+        idxs = nullptr;
+
+        update_mesh();
     }
-}
+
+    Sprite::~Sprite()
+    {
+        delete[] verts;
+        delete[] idxs;
+    }
+
+    auto Sprite::update(double dt) -> void
+    {
+        // For base: do nothing.
+    }
+
+    auto Sprite::draw() -> void
+    {
+        Rendering::TextureManager::get().bind_texture(texture);
+        mesh->draw();
+    }
+
+    auto Sprite::set_position(glm::vec2 position) -> void
+    {
+        bounds.position = position;
+        update_mesh();
+    }
+
+    auto Sprite::set_size(glm::vec2 size) -> void
+    {
+        bounds.extent = size;
+        update_mesh();
+    }
+
+    auto Sprite::set_rect(Rendering::Rectangle bnd) -> void
+    {
+        bounds = bnd;
+        update_mesh();
+    }
+
+    auto Sprite::set_selection(Rendering::Rectangle sel) -> void
+    {
+        selection = sel;
+        update_mesh();
+    }
+
+    auto Sprite::set_layer(s16 layer) -> void
+    {
+        this->layer = layer;
+        update_mesh();
+    }
+
+    auto Sprite::update_mesh() -> void
+    {
+        if (verts == nullptr)
+            verts = new Rendering::Vertex[4];
+
+        Rendering::Color white = {0xff, 0xff, 0xff, 0xff};
+
+        verts[0] = Rendering::Vertex{
+            selection.position.x, selection.position.y, white,
+            bounds.position.x, bounds.position.y, (float)layer};
+        verts[1] = Rendering::Vertex{selection.position.x + selection.extent.x,
+                                     selection.position.y,
+                                     white,
+                                     bounds.position.x + bounds.extent.x,
+                                     bounds.position.y,
+                                     (float)layer};
+        verts[2] = Rendering::Vertex{selection.position.x + selection.extent.x,
+                                     selection.position.y + selection.extent.y,
+                                     white,
+                                     bounds.position.x + bounds.extent.x,
+                                     bounds.position.y + bounds.extent.y,
+                                     (float)layer};
+        verts[3] = Rendering::Vertex{selection.position.x,
+                                     selection.position.y + selection.extent.y,
+                                     white,
+                                     bounds.position.x,
+                                     bounds.position.y + bounds.extent.y,
+                                     (float)layer};
+
+        if (idxs == nullptr)
+        {
+            idxs = new u16[6];
+            idxs[0] = 0;
+            idxs[1] = 1;
+            idxs[2] = 2;
+            idxs[3] = 2;
+            idxs[4] = 3;
+            idxs[5] = 0;
+        }
+
+        if (mesh.get() == nullptr)
+            mesh = create_scopeptr<Rendering::Mesh>(verts, 4, idxs, 6);
+        else
+        {
+            mesh->delete_data();
+            mesh->add_data(verts, 4, idxs, 6);
+        }
+    }
 
 } // namespace Stardust_Celeste::Graphics::G2D
