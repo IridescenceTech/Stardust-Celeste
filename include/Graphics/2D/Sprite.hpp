3207eadc7ffaeabd67ebaa7a00ed7b5904d05c67
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-07-15 11:44:08 -0400

    Add colored sprites

--- include/Graphics/2D/Sprite.hpp
+++ include/Graphics/2D/Sprite.hpp
@@ -2,15 +2,16 @@
 #include <Rendering/Mesh.hpp>
 #include <Utilities/Types.hpp>
 
-namespace Stardust_Celeste::Graphics::G2D
-{
+namespace Stardust_Celeste::Graphics::G2D {
 
-  class Sprite
-  {
+class Sprite {
   public:
     Sprite(u32 texture, Rendering::Rectangle bounds);
     Sprite(u32 texture, Rendering::Rectangle bounds,
            Rendering::Rectangle selection);
+    Sprite(u32 texture, Rendering::Rectangle bounds,
+           Rendering::Rectangle selection, Rendering::Color color);
+    Sprite(u32 texture, Rendering::Rectangle bounds, Rendering::Color color);
     ~Sprite();
 
     auto update(double dt) -> void;
@@ -21,6 +22,7 @@ namespace Stardust_Celeste::Graphics::G2D
     auto set_rect(Rendering::Rectangle bounds) -> void;
     auto set_selection(Rendering::Rectangle bounds) -> void;
     auto set_layer(s16 layer) -> void;
+    auto set_color(Rendering::Color color) -> void;
 
   protected:
     auto update_mesh() -> void;
@@ -28,11 +30,12 @@ namespace Stardust_Celeste::Graphics::G2D
   private:
     Rendering::Rectangle selection;
     Rendering::Rectangle bounds;
+    Rendering::Color color;
     u32 texture;
     s16 layer;
     ScopePtr<Rendering::Mesh> mesh;
     Rendering::Vertex *verts;
     u16 *idxs;
-  };
+};
 
 } // namespace Stardust_Celeste::Graphics::G2D
