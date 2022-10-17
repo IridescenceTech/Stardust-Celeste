d9e413a6f4fab49c96eb36cc09a9ff44491414b2
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2022-10-16 21:56:58 -0400

    Update Docs, Start G2D finalization

---
+++ include/Graphics/Graphics.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <Graphics/2D/AnimatedSprite.hpp>
+#include <Graphics/2D/AnimatedTilemap.hpp>
+#include <Graphics/2D/FontRenderer.hpp>
+#include <Graphics/2D/Sprite.hpp>
+#include <Graphics/2D/Tilemap.hpp>
