e0f492eddcf8fd22aa4db12d9a5197d4aacb1f93
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-12-06 09:33:20 -0500

    Use proper vulkan version

--- src/Rendering/GI/VK/VkContext.cpp
+++ src/Rendering/GI/VK/VkContext.cpp
@@ -309,7 +309,7 @@ namespace GI::detail {
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "Stardust Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_2;
+        appInfo.apiVersion = VK_API_VERSION_1_3;
 
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
