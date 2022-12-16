6a0a4916c88326e21a3dc7e2092ceaf846b68ae8
Nathan Bourgeois <iridecentrosesfall@gmail.com>
2022-12-16 02:45:26 -0500

    Controller Collection utilities

--- include/Utilities/Controllers/ControllerCollection.hpp
+++ include/Utilities/Controllers/ControllerCollection.hpp
@@ -1,9 +1,25 @@
 #pragma once
 #include <Utilities/Controllers/KeyboardController.hpp>
+#include <Utilities/Controllers/MouseController.hpp>
+#include <Utilities/Controllers/PSPController.hpp>
+#include <Utilities/Controllers/VitaController.hpp>
+#include <Utilities/Controllers/N3DSController.hpp>
 #include <Utilities/Input.hpp>
 
 namespace Stardust_Celeste::Utilities::Input {
 struct ControllerCollection {
+
+    inline static auto create_controller_collection() -> RefPtr<ControllerCollection> {
+        auto res = create_refptr<ControllerCollection>();
+        
+        res->keyboard_controller = new KeyboardController();
+        res->mouse_controller = new MouseController();
+        res->psp_controller = new PSPController();
+        res->vita_controller = new VitaController();
+        res->n3ds_controller = new N3DSController();
+
+        return res;
+    }
 
     inline auto add_controller_collection() -> void {
         Input::add_controller(keyboard_controller);
